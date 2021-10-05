cd32d57f7292ce11e0e2db02bd5d7533331f12f4
Bard <13266871263@163.com>
2021-10-05 13:25:03 +0800

    project init

--- restart.sh
+++ restart.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-path=$PWD
-
 git pull
-killall app
+killall chat
 
-cd $path/server
-nohup go run app.go &
-
+nohup go run chat.go &
 sleep 3
+
 num=$(netstat -nlpt | grep ":::23333" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    echo "run app success"
+    echo "run chat success"
 else
-    cd $path/server
     tail -n 10 nohup.out
-    echo "run app fail"
+    echo "run chat fail"
 fi
